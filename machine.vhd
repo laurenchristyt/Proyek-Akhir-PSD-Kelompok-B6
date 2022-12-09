e378c74b4e5f8f3bb16876dce1f84fc243c0ec1d
Syauqi <syauqido@gmail.com>
2022-12-09 10:05:49 +0700

    Revision

--- machine.vhd
+++ machine.vhd
@@ -1,126 +1,133 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
 
-entity state_machine is
-  port(
-    clk: in std_logic;
-    timer: in std_logic;
-    peoplecounter: in integer;
-    mode: in std_logic_vector(1 downto 0);
+ENTITY state_machine IS
+    PORT (
+        clk : IN STD_LOGIC;
+        timer : IN STD_LOGIC;
+        peoplecounter : IN INTEGER;
+        mode : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
 
-    sprayed: out std_logic;
-    purified: out std_logic_vector(1 downto 0)
-  );
-end entity;
+        sprayed : OUT STD_LOGIC;
+        purified : OUT STD_LOGIC
+    );
+END ENTITY;
 
-architecture rtl of state_machine is
-  type states is (OFF, SPRAY, REFILL, NORMAL, POWERSAVE, BOOST);
-  signal PS,NS: states;
-  signal power: std_logic;
-  signal soap: integer range 0 to 20;
+ARCHITECTURE rtl OF state_machine IS
+    TYPE states IS (OFF, SPRAY, REFILL, NORMAL, POWERSAVE, BOOST);
+    SIGNAL PS, NS : states;
+    SIGNAL power : STD_LOGIC;
+    SIGNAL soap : INTEGER RANGE 0 TO 20;
 
-   function purifier_state(mode: std_logic_vector(1 downto 0)) return states is
-        variable purifier: states;
-    begin
-        case mode is
-          when "00" => purifier := NORMAL;
-          when "01" => purifier := POWERSAVE;
-          when "10" => purifier := BOOST;
-        end case;
-        return purifier;
-    end function;
+    FUNCTION purifier_state(mode : STD_LOGIC_VECTOR(1 DOWNTO 0)) RETURN states IS
+        VARIABLE purifier : states;
+    BEGIN
+        CASE mode IS
+            WHEN "00" => purifier := NORMAL;
+            WHEN "01" => purifier := POWERSAVE;
+            WHEN "10" => purifier := BOOST;
+            WHEN others => purifier := NORMAL;
+        END CASE;
+        RETURN purifier;
+    END FUNCTION;
 
-begin
+BEGIN
 
-    -- default / startup values
-    PS <= OFF;
+    -- default / startup value
     sprayed <= '0';
-    purified <= "00";
-    
-
-    syncproc : process (clk, NS)
-    begin
-        if rising_edge(clk) then
+    purified <= '0';
+    syncproc : PROCESS (clk, NS)
+    BEGIN
+        IF rising_edge(clk) THEN
             PS <= NS;
-        end if;
-    end process;
+        END IF;
+    END PROCESS;
+    -- state transition logic
+    combproc : PROCESS (clk, PS)
+    BEGIN
+        IF rising_edge(clk) THEN
+            IF (peoplecounter > 3) THEN
+                power <= '1';
+            ELSE
+                power <= '0';
+            END IF;
 
-    
-  -- state transition logic
-  combproc : process(clk)
-  begin
-    if rising_edge(clk) then
-        if (peoplecounter >= 3) then
-            power <= '1';
-        else 
-            power <= '0';
-    end if;
+            CASE PS IS
+                    -- OFF state
+                WHEN OFF =>
+                    sprayed <= '0';
+                    purified <= '0';
+                    IF power = '1' THEN
+                        NS <= SPRAY;
+                    END IF;
 
-      case PS is
-        -- OFF state
-        when OFF =>
-          if power = '1' then
-            NS <= SPRAY;
-          end if;
+                    -- SPRAY state
+                WHEN SPRAY =>
+                    purified <= '0';
+                    IF power = '1' AND soap > 0 THEN
+                        soap <= soap - 1;
+                        sprayed <= '1';
+                        NS <= purifier_state(mode);
+                    ELSE
+                        NS <= REFILL;
+                        sprayed <= '0';
+                    END IF;
 
-        -- SPRAY state
-        when SPRAY =>
-          if power = '1' and soap >= 0 then
-            sprayed <= '1';
-            NS <= purifier_state(mode);
-          else
-            NS <= REFILL;
-          end if;
+                    -- REFILL state
+                WHEN REFILL =>
+                        sprayed <= '0';
+                        purified <= '0';
+                        soap <= soap + 1;
+                    IF (timer = '1') THEN
+                        NS <= SPRAY;
+                    ELSE 
+                        NS <= REFILL;
+                    END IF;
 
-        -- REFILL state
-        when REFILL =>
-            if (timer /= '0') then
-                soap <= soap + 1;
-            elsif (timer /= '1') then
-                NS <= SPRAY;
-            end if;
+                    -- NORMAL state
+                WHEN NORMAL =>
+                    sprayed <= '0';
+                    IF power = '1' THEN
+                        IF timer = '0' THEN
+                            purified <= '1';
+                            NS <= purifier_state(mode);
+                        ELSE
+                            NS <= SPRAY;
+                        END IF;
+                    ELSE
+                        NS <= OFF;
+                    END IF;
 
-        -- NORMAL state
-        when NORMAL =>
-          if power = '1' then
-            if timer = '0' then
-                purified <= "00";
-                NS <= purifier_state(mode);
-              else
-                NS <= SPRAY;
-              end if;
-          else
-              NS <= OFF;
-          end if;
+                    -- POWERSAVE state
+                WHEN POWERSAVE =>
+                sprayed <= '0';
+                    IF power = '1' THEN
+                        IF timer = '0' THEN
+                            purified <= '1';
+                            NS <= purifier_state(mode);
+                        ELSE
+                            NS <= SPRAY;
+                        END IF;
+                    ELSE
+                        NS <= OFF;
+                    END IF;
 
-        -- POWERSAVE state
-        when POWERSAVE =>
-        if power = '1' then
-            if timer = '0' then
-                purified <= "01";
-                NS <= purifier_state(mode);
-              else
-                NS <= SPRAY;
-              end if;
-          else
-              NS <= OFF;
-          end if;
+                    -- BOOST state
+                WHEN BOOST =>
+                sprayed <= '0';
+                    IF power = '1' THEN
+                        IF timer = '0' THEN
+                            purified <= '1';
+                            NS <= purifier_state(mode);
+                        ELSE
+                            NS <= SPRAY;
+                        END IF;
+                    ELSE
+                        NS <= OFF;
+                    END IF;               
 
-        -- BOOST state
-        when BOOST =>
-        if power = '1' then
-            if timer = '0' then
-                purified <= "10";
-                NS <= purifier_state(mode);
-              else
-                NS <= SPRAY;
-              end if;
-          else
-              NS <= OFF;
-          end if;
-
-      end case;
-    end if;
-  end process;
-end architecture;
+            END CASE;
+        END IF;
+    END PROCESS;
+END ARCHITECTURE;
